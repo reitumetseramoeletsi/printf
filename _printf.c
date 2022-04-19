fae86e082ce87e7022c5dcabb3c222ddc5d9107c
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-19 12:24:10 +0200

    Print basic printf

---
+++ _printf.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdarg.h>
+
+/**
+ * _printf - Produces output according to a format
+ * @format: a charecter string
+ *
+ * Return: number of printed charecters
+ */
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	char *sval;
+	char cval;
+	int count;
+	int i;
+
+	count = 0;
+	i = 0;
+
+	va_start(ap, format);
+	for (i = 0; *(format + i); i++)
+	{
+		if (*(format + i) != '%')
+		{
+			_putchar(*(format + i));
+			count++;
+			continue;
+		}
+
+		switch (*(format + i + 1))
+		{
+		case 'c':
+			cval = va_arg(ap, int);
+			_putchar(cval);
+			count++;
+			break;
+
+		case 's':
+			for (sval = va_arg(ap, char *); *sval; sval++)
+			{
+				_putchar(*sval);
+				count++;
+			}
+			break;
+
+		case '%':
+			_putchar(*(format + i));
+			count++;
+		}
+	}
+	va_end(ap);
+	return (count);
+}
