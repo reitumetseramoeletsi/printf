58935d681f555ddd522d160db8476229b4d0af09
Reitumetse <joycreamoeletsi@gmail.com>
2022-04-22 21:49:04 +0200

    update custom string

--- custom_string.c
+++ custom_string.c
@@ -10,17 +10,15 @@
  */
 int custom_string(va_list list)
 {
-	int count = 0, argLen, charSize = sizeof(char);
+	int count = 0, i = 0, argLen, charSize = sizeof(char);
 	unsigned int ascii;
 	char *s, *buf, *hex;
 
 	buf = malloc(64 * sizeof(char));
 	hex = malloc(64 * sizeof(char));
-
 	s = va_arg(list, char*);
 	if (s == NULL)
 		s = "(NULL)";
-
 	while (*s != '\0')
 	{
 	if ((*s > 0 && *s < 32) || *s >= 127)
@@ -29,10 +27,20 @@ int custom_string(va_list list)
 		_putchar('x');
 		ascii = (int)*s;
 		hex = fromDeci(buf, 16, ascii);
-
+		while (hex[i] != '\0')
+		{
+			i++;
+		}
 		for (argLen = 0; *hex != '\0'; argLen++, hex++, count++)
+		{
+			if (i < 3)
+			{
+				_putchar('0');
+			}
+			if (*hex > 91)
+				*hex = *hex - 32;
 			write(1, hex, charSize);
-
+		}
 		count++;
 		s++;
 	}
